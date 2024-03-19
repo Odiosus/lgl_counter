1fd161fb3e3bf001f8aa989c1760adac3432faec
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-03-19 17:03:04 +0300

    create model 'Clients' in app Clients

--- service_counter/clients/models.py
+++ service_counter/clients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Clients(models.Model):
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    surname = models.CharField(max_length=100, verbose_name='Фамилия')
+    patronymic = models.CharField(max_length=100, verbose_name='Отчество')
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.surname}, {self.email}'
